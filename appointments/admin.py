18b9e7545d92aaa1ff7ea38cd0b2977038be20d0
Mellany Linhares <mellany.linhares@academico.ifs.edu.br>
2021-05-28 04:21:11 -0300

    Configuração do Django Admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from appointments.models import VaccineLocation, Vaccine, Citizen, ServiceGroup, Appointments, AvailableAppointments, \
+    VaccinationRoom
 
-# Register your models here.
+admin.site.register(Vaccine)
+admin.site.register(VaccineLocation)
+# admin.site.register(Citizen)
+admin.site.register(ServiceGroup)
+admin.site.register(AvailableAppointments)
+admin.site.register(VaccinationRoom)
+admin.site.register(Appointments)
