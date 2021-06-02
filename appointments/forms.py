b0cca074c465a6fcf668acc4866f85ba407f26c5
Mellany Linhares <mellany.linhares@academico.ifs.edu.br>
2021-06-02 03:50:11 -0300

    Adição dos forms e views das novas seções do site

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,9 +1,36 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, password_validation, authenticate
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
+from django.forms import ModelChoiceField
+from django.contrib import messages
+
+from appointments.models import VaccineLocation, Vaccine, ServiceGroup, AvailableAppointments
+from datetime import date
 
 User = get_user_model()
+
+
+class CityModelChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.city
+
+
+class AppointmentModelChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return f"{obj.time_appointment.strftime('%H:%M')} - {obj.location.name}"
+
+
+def calculate_age(born):
+    today = date.today()
+    try:
+        birthday = born.replace(year=today.year)
+    except ValueError:
+        birthday = born.replace(year=today.year, month=born.month + 1, day=1)
+    if birthday > today:
+        return today.year - born.year - 1
+    else:
+        return today.year - born.year
 
 
 class UserAdminCreationForm(forms.ModelForm):
@@ -13,13 +40,20 @@ class UserAdminCreationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('name', 'email', 'date_birth', 'is_admin')
+        localized_fields = ('date_birth',)
 
     def clean(self):
         cleaned_data = super(UserAdminCreationForm, self).clean()
         password = cleaned_data.get('password')
         password2 = cleaned_data.get('password2')
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except ValidationError as error:
+                self.add_error('password', error)
         if password and password2 and password != password2:
-            raise ValidationError("As senhas não são iguais.")
+            self.add_error('password', "As senhas não são iguais.")
+            self.add_error('password2', "As senhas não são iguais.")
         return cleaned_data
 
     def save(self, commit=True):
@@ -36,3 +70,67 @@ class UserChangeForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('name', 'email', 'password', 'date_birth', 'is_active', 'is_admin')
+
+
+class AutoCreateUserForm(UserAdminCreationForm):
+    class Meta(UserAdminCreationForm.Meta):
+        exclude = ('is_admin',)
+        widgets = {
+            'date_birth': forms.DateInput(
+                format=('%Y-%m-%d'),
+                attrs={'class': 'form-control',
+                       'placeholder': 'Selecione uma data',
+                       'type': 'date'
+                       }),
+        }
+
+
+class CreateAppointmentForm(forms.Form):
+    def __init__(self, user, request, *args, **kwargs):
+        super(CreateAppointmentForm, self).__init__(*args, **kwargs)
+        self.request = request
+        self.user = user
+        age = calculate_age(self.user.date_birth)
+        self.fields['group'] = forms.ModelChoiceField(label='Grupo de atendimento', empty_label=None,
+                                                      queryset=ServiceGroup.objects.filter(min_age__lte=age))
+
+    date_appointment = forms.CharField(label='Dia', widget=forms.DateInput(format=('%Y-%m-%d'),
+                                                                           attrs={'class': 'form-control',
+                                                                                  'placeholder': 'Selecione uma data',
+                                                                                  'type': 'date'
+                                                                                  }))
+    vaccine = forms.ModelChoiceField(label='Vacina', empty_label=None,
+                                     queryset=Vaccine.objects.all().distinct('name'))
+    city = CityModelChoiceField(label='Cidade', empty_label=None,
+                                queryset=VaccineLocation.objects.all().distinct('city'))
+
+    def clean(self):
+        cleaned_data = super(CreateAppointmentForm, self).clean()
+        chosen_date = cleaned_data.get("date_appointment")
+        vaccine = cleaned_data.get("vaccine").name
+        group = cleaned_data.get("group").id
+        city = cleaned_data.get("city").city
+        get_appointments_date = AvailableAppointments.objects.filter(date_appointment=chosen_date,
+                                                                     location__city=city,
+                                                                     vaccine__name=vaccine,
+                                                                     vacancies__gt=0)
+        if not get_appointments_date.exists():
+            self.add_error('date_appointment', "Não existe um agendamento disponível para esses dados.")
+            messages.error(self.request, "Não existe um agendamento disponível para esses dados.")
+        return cleaned_data
+
+
+class SearchAppointmentAvailableForm(forms.Form):
+    def __init__(self, chosen_date, vaccine, id_group, city, *args, **kwargs):
+        super(SearchAppointmentAvailableForm, self).__init__(*args, **kwargs)
+        self.chosen_date = chosen_date
+        self.vaccine = vaccine
+        self.id_group = id_group
+        self.city = city
+        get_appointments_date = AvailableAppointments.objects.filter(date_appointment=chosen_date,
+                                                                     location__city=city,
+                                                                     vaccine__name=vaccine,
+                                                                     vacancies__gt=0)
+        self.fields['appointments_available'] = AppointmentModelChoiceField(label='Agendamentos disponíveis',
+                                                                            empty_label=None,
+                                                                            queryset=get_appointments_date)
--- appointments/views.py
+++ appointments/views.py
@@ -1,2 +1,114 @@
-from django.shortcuts import render
-from appointments.models import VaccineLocation
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from appointments.forms import AutoCreateUserForm, CreateAppointmentForm, SearchAppointmentAvailableForm
+from appointments.models import AvailableAppointments, Appointments, ServiceGroup
+
+
+def create_citizen(request):
+    if request.method == 'POST':
+        form = AutoCreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            u = authenticate(username=user.email, password=form.cleaned_data.get("password"))
+            login(request, u)
+            messages.success(request, "O usuário foi cadastrado com sucesso!")
+            return redirect('index')
+    else:
+        form = AutoCreateUserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'create-citizen.html', context=context)
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+    messages.success(request, "O usuário foi desconectado com sucesso!")
+    return redirect('index')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+@login_required
+def check_appointment(request):
+    qs = Appointments.objects.filter(citizen=request.user)
+    if not qs.exists():
+        if request.method == 'POST':
+            form = CreateAppointmentForm(request.user, request, request.POST)
+            if form.is_valid():
+                request.session['chosen_date'] = form.cleaned_data.get("date_appointment")
+                request.session['vaccine'] = form.cleaned_data.get("vaccine").name
+                request.session['group'] = form.cleaned_data.get("group").id
+                request.session['city'] = form.cleaned_data.get("city").city
+                return redirect('realizar_agendamento')
+        else:
+            form = CreateAppointmentForm(request.user, request)
+        print(request.path)
+        context = {
+            'form': form,
+        }
+        return render(request, 'create-appointment.html', context=context)
+    else:
+        messages.error(request, "Você já possui um agendamento feito. Não é possível fazer mais de um agendamento.")
+        return redirect('index')
+
+
+@login_required
+def create_appointment(request):
+    chosen_date = request.session['chosen_date']
+    vaccine = request.session['vaccine']
+    id_group = request.session['group']
+    city = request.session['city']
+    if request.method == 'POST':
+        form = SearchAppointmentAvailableForm(chosen_date, vaccine, id_group, city, request.POST)
+        if form.is_valid():
+            chosen_appointment = form.cleaned_data.get('appointments_available')
+            print(chosen_appointment.date_appointment)
+            group = ServiceGroup.objects.get(id=id_group)
+            a = Appointments(citizen=request.user, group=group, available=chosen_appointment)
+            with transaction.atomic():
+                chosen_appointment.vacancies -= 1
+                chosen_appointment.save(force_update=True)
+                a.save()
+            request.session.pop('chosen_date')
+            request.session.pop('vaccine')
+            request.session.pop('group')
+            request.session.pop('city')
+            request.session.modified = True
+            messages.success(request, "O agendamento foi realizado com sucesso!")
+            return redirect('index')
+    else:
+        form = SearchAppointmentAvailableForm(chosen_date, vaccine, id_group, city)
+    context = {
+        'form': form,
+    }
+    return render(request, 'create-appointment.html', context=context)
+
+
+@login_required
+def show_appointment(request):
+    query = Appointments.objects.filter(citizen=request.user)
+    if query.exists():
+        data = query[0]
+        context = {
+            'info': {
+                'Dia do agendamento': f"{data.available.date_appointment.strftime('%d/%m/%Y')} "
+                                      f"às {data.available.time_appointment.strftime('%H:%M')}",
+                'Nome do cidadão': request.user.name,
+                'Grupo de atendimento': data.group,
+                'Local de vacinação': data.available.location.name,
+                'Endereço': f"{data.available.location.address}, bairro {data.available.location.neighborhood}",
+                'Cidade': data.available.location.city
+            }
+        }
+        return render(request, 'show-appointment.html', context=context)
+    else:
+        messages.error(request, "Você ainda não possui agendamentos.")
+        return redirect('index')
